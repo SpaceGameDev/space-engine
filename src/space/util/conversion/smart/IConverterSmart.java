c58479394674f7c44b1431c7eedd6eaadd332ebc
Firestar99 <31222740+firestar99@users.noreply.github.com>
2017-11-15 16:28:29 +0100

    started work at SmartConverter

--- src/space/util/conversion/smart/IConverterSmart.java
+++ src/space/util/conversion/smart/IConverterSmart.java
@@ -1,10 +1,7 @@
 package space.util.conversion.smart;
 
-import space.util.baseobject.BaseObject;
 import space.util.conversion.Converter;
 import space.util.conversion.ConverterMap;
-import space.util.string.toStringHelper.ToStringHelper;
-import space.util.string.toStringHelper.ToStringHelper.ToStringHelperObjectsInstance;
 
 public interface IConverterSmart<MIN> extends ConverterMap<MIN, MIN> {
 	
