dde4d11ad9627ecab03fdf6710379e55eab49dd2
Firester99 <31222740+Firestar99@users.noreply.github.com>
2017-12-19 21:28:03 +0100

    - wanted to try out NumberConverter.main()... - BaseObject cinit still makes issues

--- src/space/engine/environment/Environment.java
+++ src/space/engine/environment/Environment.java
@@ -4,7 +4,7 @@ import space.util.keygen.IKey;
 import space.util.keygen.IKeyGenerator;
 import space.util.keygen.impl.IDKey.IDKey.IDKeyGenerator;
 import space.util.keygen.map.IKeyMapGeneralGeneric;
-import space.util.keygen.map.KeyMapGeneralGeneric;
+import space.util.keygen.map.KeyMapGlobalGeneric;
 import space.util.keygen.map.KeyMapKeyGeneric;
 
 public class Environment {
@@ -14,7 +14,7 @@ public class Environment {
 	public static IKeyMapGeneralGeneric<EnvFieldType> type;
 	
 	static {
-		type = new KeyMapGeneralGeneric<>();
+		type = new KeyMapGlobalGeneric<>();
 		gen = new IDKeyGenerator();
 	}
 	
