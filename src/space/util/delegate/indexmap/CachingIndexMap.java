b6f67a7f7b2ea4025b9678a596e1b1cda2f801dd
Firestar99 <31222740+Firestar99@users.noreply.github.com>
2018-03-28 18:40:30 +0200

    CachingIndexMap is done, in a more "strict" way, aka no iterating

--- src/space/util/delegate/indexmap/CachingIndexMap.java
+++ src/space/util/delegate/indexmap/CachingIndexMap.java
@@ -3,6 +3,7 @@ package space.util.delegate.indexmap;
 import space.util.baseobject.Cache;
 import space.util.baseobject.ToString;
 import space.util.delegate.map.CachingMap;
+import space.util.delegate.util.CacheUtil;
 import space.util.indexmap.IndexMap;
 import space.util.string.toStringHelper.ToStringHelper;
 import space.util.string.toStringHelper.ToStringHelper.ToStringHelperObjectsInstance;
@@ -11,196 +12,141 @@ import java.util.Collection;
 import java.util.function.IntFunction;
 import java.util.function.Supplier;
 
-import static space.util.delegate.util.CacheUtil.*;
-
 /**
  * The {@link CachingMap} tries to get a value from the {@link CachingMap#map}, and when no value has been found, it will get the value from the {@link CachingMap#def}, write it into the local map and return it;
  * <p>
  * {@link CachingMap} is threadsafe, if the internal {@link CachingMap#map} is threadsafe.
  */
-public class CachingIndexMap<VALUE> implements IndexMap<VALUE>, ToString, Cache {
+public class CachingIndexMap<VALUE> extends ConvertingIndexMap.BiDirectional<VALUE, VALUE> implements Cache, ToString {
 	
-	public IndexMap<VALUE> indexMap;
 	public IntFunction<VALUE> def;
 	
 	public CachingIndexMap(IndexMap<VALUE> indexMap, IntFunction<VALUE> def) {
-		this.indexMap = indexMap;
+		super(indexMap, CacheUtil::fromCache, CacheUtil::toCache);
 		this.def = def;
 	}
 	
 	@Override
-	public boolean isExpandable() {
-		return indexMap.isExpandable();
-	}
-	
-	@Override
-	public int size() {
-		return indexMap.size();
-	}
-	
-	@Override
-	public boolean isEmpty() {
-		return indexMap.isEmpty();
-	}
-	
-	@Override
-	public boolean contains(int index) {
-		return indexMap.putIfAbsent(index, () -> toCache(def.apply(index))) != null;
-	}
-	
-	@Override
-	@Deprecated
 	public boolean contains(VALUE v) {
 		throw new UnsupportedOperationException();
 	}
 	
 	@Override
-	public void add(VALUE v) {
-		indexMap.add(toCache(v));
-	}
-	
-	@Override
-	public VALUE get(int index) {
-		return fromCache(indexMap.putIfAbsent(index, () -> toCache(def.apply(index))));
-	}
-	
-	@Override
-	public IndexMapEntry<VALUE> getEntry(int index) {
-		IndexMapEntry<VALUE> entry = indexMap.getEntry(index);
-		entry.setIfAbsent(() -> def.apply(index));
-		return entry;
-	}
-	
-	@Override
-	public VALUE put(int index, VALUE v) {
-		return indexMap.put(index, v);
-	}
-	
-	@Override
-	@Deprecated
 	public int indexOf(VALUE v) {
 		throw new UnsupportedOperationException();
 	}
 	
 	@Override
-	public VALUE remove(int index) {
-		return indexMap.remove(index);
+	public boolean replace(int index, VALUE oldValue, VALUE newValue) {
+		boolean[] chg = new boolean[1];
+		super.putIfAbsent(index, () -> {
+			chg[0] = true;
+			VALUE defValue = def.apply(index);
+			return defValue == oldValue ? newValue : defValue;
+		});
+		return chg[0] || super.replace(index, oldValue, newValue);
 	}
 	
 	@Override
-	@Deprecated
+	public boolean replace(int index, VALUE oldValue, Supplier<? extends VALUE> newValue) {
+		boolean[] chg = new boolean[1];
+		super.putIfAbsent(index, () -> {
+			chg[0] = true;
+			VALUE defValue = def.apply(index);
+			return defValue == oldValue ? newValue.get() : defValue;
+		});
+		return chg[0] || super.replace(index, oldValue, newValue);
+	}
+	
+	@Override
+	public boolean remove(VALUE v) {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public boolean remove(int index, VALUE v) {
+		boolean[] chg = new boolean[1];
+		super.putIfAbsent(index, () -> {
+			chg[0] = true;
+			VALUE defValue = def.apply(index);
+			return defValue == v ? null : defValue;
+		});
+		return chg[0] || super.remove(index, v);
+	}
+	
+	@Override
+	public Collection<VALUE> values() {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public boolean contains(int index) {
+		return super.putIfAbsent(index, () -> this.def.apply(index)) != null;
+	}
+	
+	@Override
+	public VALUE get(int index) {
+		return super.putIfAbsent(index, () -> this.def.apply(index));
+	}
+	
+	@Override
 	public VALUE[] toArray() {
 		throw new UnsupportedOperationException();
 	}
 	
 	@Override
-	@Deprecated
-	public VALUE[] toArray(VALUE[] array) {
+	public VALUE[] toArray(VALUE[] a) {
 		throw new UnsupportedOperationException();
 	}
 	
 	@Override
-	public void addAll(Collection<VALUE> coll) {
-		indexMap.addAll(coll);
-	}
-	
-	@Override
-	public void putAll(IndexMap<VALUE> indexMap) {
-		this.indexMap.putAll(indexMap);
-	}
-	
-	@Override
-	public void putAllIfAbsent(IndexMap<VALUE> indexMap) {
-		indexMap.table().forEach(entry -> putIfAbsent(entry.getIndex(), entry::getValue));
-	}
-	
-	@Override
 	public VALUE getOrDefault(int index, VALUE def) {
-		VALUE value = indexMap.putIfAbsent(index, def);
+		VALUE value = super.putIfAbsent(index, () -> this.def.apply(index));
 		return value == null ? def : value;
 	}
-	
-	@Override
-	public VALUE putIfAbsent(int index, VALUE v) {
-		indexMap.putIfAbsent(index, () -> def.apply(index));
-		return indexMap.putIfAbsent(index, v);
-	}
-	
-	@Override
-	public VALUE putIfAbsent(int index, Supplier<? extends VALUE> v) {
-		return indexMap.putIfAbsent(index, v);
-	}
-	
-	@Override
-	public boolean replace(int index, VALUE oldValue, VALUE newValue) {
-		return indexMap.replace(index, oldValue, newValue);
-	}
-	
-	@Override
-	public boolean replace(int index, VALUE oldValue, Supplier<? extends VALUE> newValue) {
-		return indexMap.replace(index, oldValue, newValue);
-	}
-	
-	@Override
-	public boolean remove(VALUE v) {
-		return indexMap.remove(v);
-	}
-	
-	@Override
-	public boolean remove(int index, VALUE v) {
-		return indexMap.remove(index, v);
-	}
-	
-	@Override
-	public void clear() {
-		indexMap.clear();
-	}
-	
-	@Override
-	public Collection<VALUE> values() {
-		return indexMap.values();
-	}
-	
-	@Override
-	public Collection<IndexMapEntry<VALUE>> table() {
-		return indexMap.table();
-	}
-//	//get
-//	@Override
-//	public VALUE get(int index) {
-//		VALUE thisV = indexMap.get(index);
-//		if (thisV != null)
-//			return fromCache(thisV);
-//
-//		VALUE newV = def.apply(index);
-//		put(index, toCache(newV));
-//		return newV;
-//	}
-//
-//	@Override
-//	public boolean replace(int index, VALUE oldValue, VALUE newValue) {
-//		IndexMapEntry<VALUE> entry = indexMap.getEntry(index);
-//		VALUE value = entry.getValue();
-//		boolean useDef = value == null;
-//
-//		if (useDef)
-//			value = this.def.apply(index);
-//		if (Objects.equals(oldValue, value)) {
-//			entry.setValue(newValue);
-//			return true;
-//		}
-//		if (useDef)
-//			entry.setValue(value);
-//		return false;
-//	}
 	
 	@Override
 	public <TSHTYPE> TSHTYPE toTSH(ToStringHelper<TSHTYPE> api) {
 		ToStringHelperObjectsInstance<TSHTYPE> tsh = api.createObjectInstance(this);
 		tsh.add("indexMap", this.indexMap);
 		tsh.add("def", this.def);
-		tsh.add("iterateOverDef", this.iterateOverDef);
 		return tsh.build();
+	}
+	
+	@Override
+	public IndexMapEntry<VALUE> getEntry(int index) {
+		IndexMapEntry<VALUE> entry = super.getEntry(index);
+		entry.setIfAbsent(() -> this.def.apply(index));
+		return entry;
+	}
+	
+	@Override
+	public void putAllIfAbsent(IndexMap<VALUE> indexMap) {
+		indexMap.table().forEach(entry -> super.putIfAbsent(entry.getIndex(), () -> {
+			VALUE defValue = def.apply(entry.getIndex());
+			return defValue != null ? defValue : entry.getValue();
+		}));
+	}
+	
+	@Override
+	public VALUE putIfAbsent(int index, VALUE v) {
+		return super.putIfAbsent(index, () -> {
+			VALUE defValue = def.apply(index);
+			return defValue != null ? defValue : v;
+		});
+	}
+	
+	@Override
+	public VALUE putIfAbsent(int index, Supplier<? extends VALUE> v) {
+		return super.putIfAbsent(index, () -> {
+			VALUE defValue = def.apply(index);
+			return defValue != null ? defValue : v.get();
+		});
+	}
+	
+	@Override
+	public Collection<IndexMapEntry<VALUE>> table() {
+		throw new UnsupportedOperationException();
 	}
 	
 	@Override
