eee4639ea506d440f2333fb503c82db0209b7e97
Firestar99 <31222740+firestar99@users.noreply.github.com>
2019-01-16 15:06:19 +0100

    [Task] added LotsOfObjectsTest: many objects with cartesian product transactions

---
+++ src/main/java/space/util/task/test/LotsOfObjectsTest.java
@@ -0,0 +1,112 @@
+package space.util.task.test;
+
+import org.jetbrains.annotations.NotNull;
+import space.util.sync.barrier.Barrier;
+import space.util.sync.barrier.BarrierImpl;
+import space.util.task.TaskCreator;
+import space.util.task.impl.MultiTask;
+import space.util.task.test.TransactionTest.Entity;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+import static space.util.Global.GLOBAL_EXECUTOR;
+import static space.util.task.Tasks.parallel;
+
+public class LotsOfObjectsTest {
+	
+	public static final int[] OBJECT_COUNT = new int[] {500, 1000, 2000};
+	public static boolean FANCY_PRINTOUT = true;
+	public static boolean TIMER_PRINTOUT = true;
+	
+	public static void main(String[] args) throws InterruptedException {
+		try {
+			GLOBAL_EXECUTOR = Executors.newFixedThreadPool(8);
+			System.out.print(""); //initialization
+			
+			//run
+			for (int count : OBJECT_COUNT) {
+				System.out.println(String.format("%1$3s", count) + ": " + formatTimeMs(run(count)));
+			}
+		} finally {
+			if (GLOBAL_EXECUTOR instanceof ThreadPoolExecutor)
+				((ThreadPoolExecutor) GLOBAL_EXECUTOR).shutdown();
+		}
+	}
+	
+	private static class IntVector {
+		
+		public int x;
+		public int y;
+		
+		public IntVector(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+	
+	private static long run(int objectsCount) throws InterruptedException {
+		Entity[] world = IntStream.range(0, objectsCount).mapToObj(v -> new Entity()).toArray(Entity[]::new);
+		long time;
+		
+		if (FANCY_PRINTOUT)
+			System.out.println(objectsCount + " Objects: taskCreator");
+		time = System.nanoTime();
+		TaskCreator<? extends MultiTask> taskCreator =
+				parallel(IntStream.range(0, world.length)
+								  .boxed()
+								  .flatMap(x -> IntStream.range(0, world.length)
+														 .mapToObj(y -> new IntVector(x, y))
+														 .filter(v -> v.x != v.y))
+								  .map(v -> TransactionTest.createTransaction(world[v.x], world[v.y]))
+								  .collect(Collectors.toList()));
+		if (TIMER_PRINTOUT)
+			System.out.println(formatTimeMs(System.nanoTime() - time));
+		
+		BarrierImpl barrier = new BarrierImpl();
+		
+		if (FANCY_PRINTOUT)
+			System.out.println(objectsCount + " Objects: submitting");
+		long totalTime = time = System.nanoTime();
+		Barrier task = taskCreator.submit(barrier);
+		if (TIMER_PRINTOUT)
+			System.out.println(formatTimeMs(System.nanoTime() - time));
+		
+		if (FANCY_PRINTOUT)
+			System.out.println(objectsCount + " Objects: launching");
+		time = System.nanoTime();
+		barrier.triggerNow();
+		if (TIMER_PRINTOUT)
+			System.out.println(formatTimeMs(System.nanoTime() - time));
+		
+		if (FANCY_PRINTOUT)
+			System.out.println(objectsCount + " Objects: awaiting");
+		time = System.nanoTime();
+		task.await();
+		if (TIMER_PRINTOUT)
+			System.out.println(formatTimeMs(System.nanoTime() - time));
+		
+		if (FANCY_PRINTOUT)
+			System.out.println(objectsCount + " Objects: done!");
+		
+		for (Entity entity : world)
+			if (entity.count != 0)
+				throw new RuntimeException();
+		
+		long totalDelta = System.nanoTime() - totalTime;
+		if (TIMER_PRINTOUT)
+			System.out.println("total execution time: " + formatTimeMs(totalDelta));
+		return totalDelta;
+	}
+	
+	@NotNull
+	private static String formatTimeMs(long time) {
+		return String.format("%1$3s", NANOSECONDS.toSeconds(time)) + "," +
+				String.format("%1$3s", NANOSECONDS.toMillis(time) % 1000) + "." +
+				String.format("%1$3s", NANOSECONDS.toMicros(time) % 1000) + "." +
+				String.format("%1$3s", NANOSECONDS.toNanos(time) % 1000) + "ms";
+	}
+}
