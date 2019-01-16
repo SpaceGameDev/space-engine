7e5f5d6f122e55fdaa5bf53467c28f7f29b07176
Firestar99 <31222740+firestar99@users.noreply.github.com>
2019-01-16 15:09:48 +0100

    [Task] improved SyncLock unlocking

--- src/main/java/space/util/sync/lock/SyncLockImpl.java
+++ src/main/java/space/util/sync/lock/SyncLockImpl.java
@@ -1,12 +1,15 @@
 package space.util.sync.lock;
 
 import org.jetbrains.annotations.NotNull;
+import space.util.Global;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BooleanSupplier;
 
 public class SyncLockImpl implements SyncLock {
+	
+	public static int SYNCLOCK_CALLBACK_TRIES = 2;
 	
 	private boolean locked;
 	private int modId;
@@ -38,36 +41,48 @@ public class SyncLockImpl implements SyncLock {
 			modId = this.modId;
 		}
 		
-		return () -> {
-			BooleanSupplier callback;
-			while (true) {
-				//lock and get callback
-				synchronized (this) {
-					if (locked || modId != this.modId)
-						return;
-					if (notifyUnlock.isEmpty()) {
-						//no callback found to accept lock
-						return;
-					}
-					callback = notifyUnlock.remove(0);
-					locked = true;
-				}
-				
-				//call callback
-				if (callback.getAsBoolean()) {
-					//accepted lock
-					return;
-				}
-				
-				//unlock
-				synchronized (this) {
-					locked = false;
-				}
-				
-				//possible to better prevent lifelocks though performance advantage negligible; only at extreme transaction counts (> 100,000)
-//				Thread.yield();
+		return () -> unlockRunCallbackOnce(modId);
+	}
+	
+	private void unlockRunCallbackTry(final int modid) {
+		for (int i = 0; i < SYNCLOCK_CALLBACK_TRIES; i++) {
+			if (unlockRunCallbackOnce(modId))
+				return;
+			
+			//possible to better prevent lifelocks though performance advantage negligible; only at extreme transaction counts (> 100,000)
+//			Thread.yield();
+		}
+		
+		//out of tries -> enqueue and try again later
+		Global.GLOBAL_EXECUTOR.execute(() -> unlockRunCallbackTry(modid));
+	}
+	
+	private boolean unlockRunCallbackOnce(int modId) {
+		BooleanSupplier callback;
+		
+		//lock and get callback
+		synchronized (this) {
+			if (locked || modId != this.modId)
+				return true;
+			if (notifyUnlock.isEmpty()) {
+				//no callback found to accept lock
+				return true;
 			}
-		};
+			callback = notifyUnlock.remove(0);
+			locked = true;
+		}
+		
+		//call callback
+		if (callback.getAsBoolean()) {
+			//accepted lock
+			return true;
+		}
+		
+		//unlock
+		synchronized (this) {
+			locked = false;
+		}
+		return false;
 	}
 	
 	@Override
