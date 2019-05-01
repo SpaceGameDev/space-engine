93eb0ad147ad95780245937ab27eadf4a314589c
Firestar99 <31222740+firestar99@users.noreply.github.com>
2019-05-01 17:04:23 +0200

    [vulkan] added VkEvent and VkFence

---
+++ space-engine-vulkan/src/main/java/space/engine/vulkan/VkFence.java
@@ -0,0 +1,89 @@
+package space.engine.vulkan;
+
+import org.jetbrains.annotations.NotNull;
+import org.lwjgl.vulkan.VkFenceCreateInfo;
+import space.engine.buffer.Allocator;
+import space.engine.buffer.AllocatorStack.AllocatorFrame;
+import space.engine.buffer.pointer.PointerBufferPointer;
+import space.engine.freeableStorage.Freeable;
+import space.engine.freeableStorage.Freeable.FreeableWrapper;
+import space.engine.freeableStorage.FreeableStorage;
+import space.engine.sync.barrier.Barrier;
+
+import java.util.function.BiFunction;
+
+import static org.lwjgl.vulkan.VK10.*;
+import static space.engine.freeableStorage.Freeable.addIfNotContained;
+import static space.engine.vulkan.VkException.assertVk;
+
+public class VkFence implements FreeableWrapper {
+	
+	//alloc
+	public static @NotNull VkFence alloc(VkFenceCreateInfo info, @NotNull VkDevice device, @NotNull Object[] parents) {
+		try (AllocatorFrame frame = Allocator.frame()) {
+			PointerBufferPointer ptr = PointerBufferPointer.malloc(frame);
+			assertVk(nvkCreateFence(device, info.address(), 0, ptr.address()));
+			return create(ptr.getPointer(), device, parents);
+		}
+	}
+	
+	//create
+	public static @NotNull VkFence create(long address, @NotNull VkDevice device, @NotNull Object[] parents) {
+		return new VkFence(address, device, Storage::new, parents);
+	}
+	
+	public static @NotNull VkFence wrap(long address, @NotNull VkDevice device, @NotNull Object[] parents) {
+		return new VkFence(address, device, Freeable::createDummy, parents);
+	}
+	
+	//const
+	public VkFence(long address, @NotNull VkDevice device, @NotNull BiFunction<VkFence, Object[], Freeable> storageCreator, @NotNull Object[] parents) {
+		this.address = address;
+		this.device = device;
+		this.storage = storageCreator.apply(this, addIfNotContained(parents, device));
+	}
+	
+	//parents
+	private final @NotNull VkDevice device;
+	
+	public VkDevice device() {
+		return device;
+	}
+	
+	public VkInstance instance() {
+		return device.instance();
+	}
+	
+	//address
+	private final long address;
+	
+	public long address() {
+		return address;
+	}
+	
+	//storage
+	private final @NotNull Freeable storage;
+	
+	@Override
+	public @NotNull Freeable getStorage() {
+		return storage;
+	}
+	
+	public static class Storage extends FreeableStorage {
+		
+		private final @NotNull VkDevice device;
+		private final long address;
+		
+		public Storage(@NotNull VkFence o, @NotNull Object[] parents) {
+			super(o, parents);
+			this.device = o.device;
+			this.address = o.address;
+		}
+		
+		@Override
+		protected @NotNull Barrier handleFree() {
+			vkDestroyFence(device, address, null);
+			return Barrier.ALWAYS_TRIGGERED_BARRIER;
+		}
+	}
+}
