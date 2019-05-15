d5944097e226cb3b0a523c60eb0b8f7ede237380
Firestar99 <31222740+firestar99@users.noreply.github.com>
2019-05-15 20:04:14 +0200

    [baseobject] deleted some unused Classes

--- src/main/java/space/engine/baseobject/Setable.java
+++
@@ -1,9 +0,0 @@
-package space.engine.baseobject;
-
-import org.jetbrains.annotations.NotNull;
-import space.engine.baseobject.exceptions.InvalidSetException;
-
-public interface Setable {
-	
-	void set(@NotNull Object obj) throws InvalidSetException;
-}
