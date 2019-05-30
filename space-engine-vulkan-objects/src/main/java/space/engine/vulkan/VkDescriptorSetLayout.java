7eaa8079c496fe2dde4af661d12d7f7fb9ceafd8
Firestar99 <31222740+firestar99@users.noreply.github.com>
2019-05-30 18:48:06 +0200

    [vulkan] alloc() methods creating infos themselves

--- space-engine-vulkan-objects/src/main/java/space/engine/vulkan/VkDescriptorSetLayout.java
+++ space-engine-vulkan-objects/src/main/java/space/engine/vulkan/VkDescriptorSetLayout.java
@@ -1,6 +1,8 @@
 package space.engine.vulkan;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.lwjgl.vulkan.VkDescriptorSetLayoutBinding;
 import org.lwjgl.vulkan.VkDescriptorSetLayoutCreateInfo;
 import space.engine.buffer.Allocator;
 import space.engine.buffer.AllocatorStack.AllocatorFrame;
@@ -14,11 +16,23 @@ import java.util.function.BiFunction;
 
 import static org.lwjgl.vulkan.VK10.*;
 import static space.engine.freeableStorage.Freeable.addIfNotContained;
+import static space.engine.lwjgl.LwjglStructAllocator.mallocStruct;
 import static space.engine.vulkan.VkException.assertVk;
 
 public class VkDescriptorSetLayout implements FreeableWrapper {
 	
 	//alloc
+	public static @NotNull VkDescriptorSetLayout alloc(@NotNull VkDevice device, int flags, @Nullable VkDescriptorSetLayoutBinding.Buffer bindings, @NotNull Object[] parents) {
+		try (AllocatorFrame frame = Allocator.frame()) {
+			return alloc(mallocStruct(frame, VkDescriptorSetLayoutCreateInfo::create, VkDescriptorSetLayoutCreateInfo.SIZEOF).set(
+					VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO,
+					0,
+					flags,
+					bindings
+			), device, parents);
+		}
+	}
+	
 	public static @NotNull VkDescriptorSetLayout alloc(VkDescriptorSetLayoutCreateInfo info, @NotNull VkDevice device, @NotNull Object[] parents) {
 		try (AllocatorFrame frame = Allocator.frame()) {
 			PointerBufferPointer ptr = PointerBufferPointer.malloc(frame);
--- space-engine-vulkan-objects/src/main/java/space/engine/vulkan/VkPipelineLayout.java
+++ space-engine-vulkan-objects/src/main/java/space/engine/vulkan/VkPipelineLayout.java
@@ -4,22 +4,37 @@ import org.jetbrains.annotations.NotNull;
 import org.lwjgl.vulkan.VkPipelineLayoutCreateInfo;
 import space.engine.buffer.Allocator;
 import space.engine.buffer.AllocatorStack.AllocatorFrame;
+import space.engine.buffer.array.ArrayBufferLong;
 import space.engine.buffer.pointer.PointerBufferPointer;
 import space.engine.freeableStorage.Freeable;
 import space.engine.freeableStorage.Freeable.FreeableWrapper;
 import space.engine.freeableStorage.FreeableStorage;
 import space.engine.sync.barrier.Barrier;
 
+import java.util.Arrays;
 import java.util.function.BiFunction;
 
 import static org.lwjgl.vulkan.VK10.*;
 import static space.engine.freeableStorage.Freeable.addIfNotContained;
+import static space.engine.lwjgl.LwjglStructAllocator.mallocStruct;
 import static space.engine.vulkan.VkException.assertVk;
 
 public class VkPipelineLayout implements FreeableWrapper {
 	
 	//alloc
-	public static @NotNull VkPipelineLayout alloc(VkPipelineLayoutCreateInfo info, @NotNull VkDescriptorSetLayout[] descriptorSetLayouts, @NotNull VkDevice device, @NotNull Object[] parents) {
+	public static @NotNull VkPipelineLayout alloc(@NotNull VkDevice device, @NotNull VkDescriptorSetLayout[] descriptorSetLayouts, @NotNull Object[] parents) {
+		try (AllocatorFrame frame = Allocator.frame()) {
+			return alloc(mallocStruct(frame, VkPipelineLayoutCreateInfo::create, VkPipelineLayoutCreateInfo.SIZEOF).set(
+					VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO,
+					0,
+					0,
+					ArrayBufferLong.alloc(frame, Arrays.stream(descriptorSetLayouts).mapToLong(VkDescriptorSetLayout::address).toArray()).nioBuffer(),
+					null
+			), device, descriptorSetLayouts, parents);
+		}
+	}
+	
+	public static @NotNull VkPipelineLayout alloc(VkPipelineLayoutCreateInfo info, @NotNull VkDevice device, @NotNull VkDescriptorSetLayout[] descriptorSetLayouts, @NotNull Object[] parents) {
 		try (AllocatorFrame frame = Allocator.frame()) {
 			PointerBufferPointer ptr = PointerBufferPointer.malloc(frame);
 			assertVk(nvkCreatePipelineLayout(device, info.address(), 0, ptr.address()));
