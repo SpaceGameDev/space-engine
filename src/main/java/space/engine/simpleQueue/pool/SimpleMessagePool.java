9754c93015bde5b47c1bf19cc3417532b252d5e2
Firestar99 <31222740+firestar99@users.noreply.github.com>
2019-06-03 13:50:48 +0200

    [SimpleQueue] fixed deadlock: SimpleMessagePool not unpacking Threads properly

--- src/main/java/space/engine/simpleQueue/pool/SimpleMessagePool.java
+++ src/main/java/space/engine/simpleQueue/pool/SimpleMessagePool.java
@@ -3,44 +3,33 @@ package space.engine.simpleQueue.pool;
 import org.jetbrains.annotations.NotNull;
 import space.engine.simpleQueue.SimpleQueue;
 
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodHandles.Lookup;
-import java.lang.invoke.VarHandle;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.locks.LockSupport;
 
 public abstract class SimpleMessagePool<MSG> extends AbstractSimpleMessagePool<MSG> {
 	
-	private static final VarHandle THREADISSLEEPING;
-	
-	static {
-		try {
-			Lookup lookup = MethodHandles.lookup();
-			THREADISSLEEPING = lookup.findVarHandle(SimpleMessagePool.class, "threadIsSleeping", boolean.class);
-		} catch (NoSuchFieldException | IllegalAccessException e) {
-			throw new ExceptionInInitializerError(e);
-		}
-	}
-	
-	private volatile boolean threadIsSleeping = false;
-	
 	public SimpleMessagePool(int threadCnt, @NotNull SimpleQueue<MSG> queue) {
-		super(threadCnt, queue);
+		this(threadCnt, queue, Executors.defaultThreadFactory());
 	}
 	
 	public SimpleMessagePool(int threadCnt, @NotNull SimpleQueue<MSG> queue, ThreadFactory threadFactory) {
-		super(threadCnt, queue, threadFactory);
+		this(threadCnt, queue, threadFactory, true);
+	}
+	
+	protected SimpleMessagePool(int threadCnt, @NotNull SimpleQueue<MSG> queue, ThreadFactory threadFactory, boolean callinit) {
+		super(threadCnt, queue, threadFactory, callinit);
 	}
 	
 	//park
-	protected void park() {
-		THREADISSLEEPING.set(this, true);
-		LockSupport.park();
+	protected synchronized void park() {
+		try {
+			this.wait();
+		} catch (InterruptedException ignored) {
+		
+		}
 	}
 	
-	protected void unparkThreads() {
-		if (THREADISSLEEPING.compareAndSet(this, true, false))
-			for (Thread thread : threads)
-				LockSupport.unpark(thread);
+	protected synchronized void unparkThreads() {
+		this.notifyAll();
 	}
 }
