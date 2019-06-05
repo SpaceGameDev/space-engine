09b692c29d3bb08c142f698944bdfdec2d1d672a
Firestar99 <31222740+firestar99@users.noreply.github.com>
2019-06-05 20:11:46 +0200

    [vulkan] better doc for VkMappedBuffer.uploadData()

--- space-engine-vulkan-objects/src/main/java/space/engine/vulkan/VkMappedBuffer.java
+++ space-engine-vulkan-objects/src/main/java/space/engine/vulkan/VkMappedBuffer.java
@@ -1,9 +1,24 @@
 package space.engine.vulkan;
 
 import space.engine.buffer.Buffer;
+import space.engine.sync.barrier.Barrier;
 
 public interface VkMappedBuffer extends VkBuffer {
 	
 	//mapping
 	Buffer mapMemory(Object[] parents);
+	
+	/**
+	 * always completes when this Method returns -> always returns {@link Barrier#ALWAYS_TRIGGERED_BARRIER}
+	 */
+	@Override
+	default Barrier uploadData(Buffer src) {
+		return uploadData(src, 0, 0, src.sizeOf());
+	}
+	
+	/**
+	 * always completes when this Method returns -> always returns {@link Barrier#ALWAYS_TRIGGERED_BARRIER}
+	 */
+	@Override
+	Barrier uploadData(Buffer src, long srcOffset, long dstOffset, long length);
 }
